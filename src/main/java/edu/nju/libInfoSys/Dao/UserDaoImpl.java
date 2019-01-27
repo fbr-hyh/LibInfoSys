ae9945e3def3cd6cdf508d825df95a4241f79626
161250041 <161250041@smail.nju.edu.cn>
2019-01-27 18:43:19 +0800

    UserDaoIml UserInfoDaoIml

--- src/main/java/edu/nju/libInfoSys/Dao/UserDaoImpl.java
+++ src/main/java/edu/nju/libInfoSys/Dao/UserDaoImpl.java
@@ -1,12 +1,34 @@
 package edu.nju.libInfoSys.Dao;
 
 import edu.nju.libInfoSys.Entity.User.Administrator;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 
 public class UserDaoImpl implements UserDao {
+    @Autowired
+    protected SessionFactory sessionFactory;
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
     @Override
     public ArrayList<Administrator> getAllAdministrator() {
-        return null;
+        Session session = getSession();
+
+        ArrayList<Administrator> administrators = new ArrayList<>();
+        try {
+            Query query=session.createQuery("from UserEntity u where u.userType=?1");
+            query.setParameter(1, 4);
+            administrators = (ArrayList<Administrator>) query.getResultList();
+        } catch (Exception e) {
+            System.out.println("无管理员");
+            return administrators;
+        }
+        return administrators;
     }
 }
--- src/main/java/edu/nju/libInfoSys/Dao/UserInfoDaoImpl.java
+++ src/main/java/edu/nju/libInfoSys/Dao/UserInfoDaoImpl.java
@@ -1,10 +1,20 @@
 package edu.nju.libInfoSys.Dao;
 
 import edu.nju.libInfoSys.Entity.RecordEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 
 public class UserInfoDaoImpl implements UserInfoDao {
+    @Autowired
+    protected SessionFactory sessionFactory;
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
 
 //    @Override
 //    public ArrayList<RecordEntity> getAllReports() {
@@ -14,18 +24,45 @@ public class UserInfoDaoImpl implements UserInfoDao {
     //该user所有借书记录
     @Override
     public ArrayList<RecordEntity> getReportsByUserId(String userId) {
-        return null;
+        Session session = getSession();
+        ArrayList<RecordEntity> recordEntities = new ArrayList<>();
+        try {
+            Query query=session.createQuery("from RecordEntity r where r.userId=?1");
+            query.setParameter(1, userId);
+            recordEntities = (ArrayList<RecordEntity>) query.getResultList();
+        } catch (Exception e) {
+            System.out.println("该用户无借书记录");
+            return recordEntities;
+        }
+        return recordEntities;
     }
 
     //该user的仍未缴费的记录 status=1
     @Override
     public ArrayList<RecordEntity> getOverdueReportsByUserId(String userId) {
-        return null;
+        Session session = getSession();
+        ArrayList<RecordEntity> recordEntities = new ArrayList<>();
+        try {
+            Query query=session.createQuery("from RecordEntity r where r.userId=?1 and r.recordStatus=?2");
+            query.setParameter(1, userId);
+            query.setParameter(2, 1);//欠费状态
+            recordEntities = (ArrayList<RecordEntity>) query.getResultList();
+        } catch (Exception e) {
+            System.out.println("该用户暂无欠费");
+            return recordEntities;
+        }
+        return recordEntities;
     }
 
     //总欠费
     @Override
     public double getOverduePenaltyByUserId(String userId) {
-        return 0;
+        Session session = getSession();
+        double overdue=0;
+        Query query=session.createQuery("select sum(r.overduePenalty) from RecordEntity r where r.userId=?1 and r.recordStatus=?2");
+        query.setParameter(1, userId);
+        query.setParameter(2, 1);//欠费状态
+        overdue = Double.parseDouble(query.getSingleResult().toString());
+        return overdue;
     }
 }
