109706179500598d2349bb80cc4e0b7f812ba8cc
FBRFL <835421460@qq.com>
2019-01-27 18:53:46 +0800

    BookOperationDaoImpl RecordDaoImpl

--- src/main/java/edu/nju/libInfoSys/Dao/RecordDaoImpl.java
+++ src/main/java/edu/nju/libInfoSys/Dao/RecordDaoImpl.java
@@ -1,32 +1,104 @@
 package edu.nju.libInfoSys.Dao;
 
 import edu.nju.libInfoSys.Entity.RecordEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.Timestamp;
 
 public class RecordDaoImpl implements RecordDao {
+    @Autowired
+    protected SessionFactory sessionFactory;
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
     @Override
     public boolean add(String bookId, String userId, Timestamp borrowTime) {
+
         return false;
     }
 
     @Override
     public RecordEntity getNotReturnRecord(String bookId, String userId) { //status=0
-        return null;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "from RecordEntity where bookId=:book and userId=:user and recordStatus=0";
+        Query query = session.createQuery(hql);
+        query.setParameter("book", bookId);
+        query.setParameter("user", userId);
+
+        if (query.list().size() == 0) {
+            transaction.commit();
+            return null;
+        }
+        RecordEntity result = (RecordEntity) query.getSingleResult();
+
+        transaction.commit();
+        return result;
     }
 
     @Override
     public boolean setReturnedOverdueAndStatus(int recordId, Timestamp returnTime, double overduePen, int status) {
-        return false;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "from RecordEntity where recordId=:id";
+        Query query = session.createQuery(hql);
+        query.setParameter("id", recordId);
+
+        if (query.list().size() == 0) {
+            transaction.commit();
+            return false;
+        }
+        RecordEntity result = (RecordEntity) query.getSingleResult();
+        result.setReturnTime(returnTime);
+        result.setOverduePenalty(overduePen);
+        result.setRecordStatus(status);
+        session.update(result);
+
+        transaction.commit();
+        return true;
     }
 
     @Override
     public boolean setOverdue(String recordId, double overdue) {
-        return false;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "from RecordEntity where recordId=:id";
+        Query query = session.createQuery(hql);
+        query.setParameter("id", recordId);
+
+        if (query.list().size() == 0) {
+            transaction.commit();
+            return false;
+        }
+        RecordEntity result = (RecordEntity) query.getSingleResult();
+        result.setOverduePenalty(overdue);
+        session.update(result);
+
+        transaction.commit();
+        return true;
     }
 
     @Override
     public int getBorrowingNumByUserId(String userId) {
-        return 0;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "from RecordEntity where userId=:user";
+        Query query = session.createQuery(hql);
+        query.setParameter("user", userId);
+
+
+        int result = query.list().size();
+        transaction.commit();
+        return result;
     }
 }
